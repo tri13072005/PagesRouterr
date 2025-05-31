f2ad168b8204076f966b12bd69756f20c0dde83d
Nguyen Tri <dontringuyen10@gmail.com>
2025-05-31 22:23:12 +0700

    Refactor blog application: update package.json for turbopack and dependencies, enhance homepage with links and date formatting, improve post page structure with static paths, and remove unused first post page.

--- PagesRouter/nextjs-blog/pages/index.js
+++ PagesRouter/nextjs-blog/pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link';
+import Date from '../components/date';
 
 export default function Home({ allPostsData }) {
   return (
@@ -10,18 +12,22 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi I'm a student at VNUK university. Im trying to be a IT full stack</p>
+        <p>[Your Self Introduction]</p>
+        <p>
+          (Hi Im Ki. Im trying to be a IT full stack{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
